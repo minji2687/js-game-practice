d283ffbf5a445bd093d94ceea285d02bdfde0edb
Jo Minji <mingi2687@gamil.com>
2020-09-25 12:05:43 +0900

    - 게임 필드 만들기(당근과 벌레 랜덤 배치) - 게임 시작 하기 - 게임 타이머 시작하기

---
+++ carrot/main.js
@@ -0,0 +1,97 @@
+"use strict";
+const field = document.querySelector(".game__field");
+const fieldRect = field.getBoundingClientRect();
+const CARROT_SIZE = 80;
+const CARROUT_COUNT = 5;
+const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;
+
+const gameBtn = document.querySelector(".game__button");
+const gameTimer = document.querySelector(".game__timer");
+const gameScore = document.querySelector(".game__score");
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener("click", () => {
+  if (started) {
+    stopGame();
+  } else {
+    startGame();
+  }
+  started = !started;
+});
+
+function startGame() {
+  initGame();
+  showStopButton();
+  showTimerAndScore();
+  startGameTimer();
+}
+function stopGame() {}
+
+function showStopButton() {
+  const icon = gameBtn.querySelector(".fas");
+  icon.classList.remove("fa-play");
+  icon.classList.add("fa-stop");
+}
+
+function showTimerAndScore() {
+  gameScore.style.visibility = "visible";
+  gameTimer.style.visibility = "visible";
+}
+
+function startGameTimer() {
+  let remainingTimeSec = GAME_DURATION_SEC;
+  updateTimerText(remainingTimeSec);
+
+  timer = setInterval(() => {
+    if (remainingTimeSec <= 0) {
+      clearInterval(timer);
+      return;
+    }
+    updateTimerText(--remainingTimeSec);
+  }, 1000);
+}
+
+function updateTimerText(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  gameTimer.innerText = `${minutes} : ${seconds}`;
+}
+
+function initGame() {
+  field.innerHTML = "";
+  gameScore.innerText = CARROUT_COUNT;
+  addItem("carrot", CARROUT_COUNT, "img/carrot.png");
+  addItem("bug", BUG_COUNT, "img/bug.png");
+}
+
+function addItem(className, count, imgPath) {
+  const x1 = 0;
+  const y1 = 0;
+  const x2 = fieldRect.width - CARROT_SIZE;
+  const y2 = fieldRect.height - CARROT_SIZE;
+
+  for (let i = 0; i < count; i++) {
+    const item = document.createElement("img");
+    item.setAttribute("class", className);
+    item.setAttribute("src", imgPath);
+
+    item.style.position = "absolute";
+
+    const x = RandomNumber(x1, x2);
+    const y = RandomNumber(y1, y2);
+    item.style.left = `${x}px`;
+    item.style.top = `${y}px`;
+
+    field.appendChild(item);
+  }
+}
+
+function RandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+initGame();
