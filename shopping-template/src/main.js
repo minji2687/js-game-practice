c53a2080eabff701cb4c9f565954c3bc91a238c7
Jo Minji <mingi2687@gamil.com>
2020-11-08 15:29:56 +0900

    리팩토링

--- shopping-template/src/main.js
+++ shopping-template/src/main.js
@@ -1,47 +1,51 @@
 function loadItems() {
-  return fetch("data/data.json")
+  return fetch("../data/data.json") //
     .then((response) => response.json())
-    .then((json) => json.items);
+    .then((result) => result.items);
 }
 
-//Update the list with the given items
-function displayItems(items) {
+function displayAllItems(items) {
   const container = document.querySelector(".items");
-  console.log(items.map((item) => createHTMLString(item)).join(""));
   container.innerHTML = items.map((item) => createHTMLString(item)).join("");
 }
 
 function createHTMLString(item) {
   return `
-    <li class="item">
-        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
-        <span class="item__description">${item.gender}, ${item.size}</span>
-    </li> `;
-}
-
-function onButtonClick(event, items) {
-  const dataset = event.target.dataset;
-  const key = dataset.key;
-  const value = dataset.value;
-
-  if (key == null || value == null) {
-    return;
-  }
-
-  const filtered = items.filter((item) => item[key] === value);
-  displayItems(filtered);
+  <li class="item">
+    <img class="item__thumbnail" src=${item.image}  alt=${item.type} />
+    <span class="item__description">${item.size}, ${item.gender}</span>
+  </li>
+`;
 }
 
 function setEventListener(items) {
   const logo = document.querySelector(".logo");
   const buttons = document.querySelector(".buttons");
-  logo.addEventListener("click", () => displayItems(items));
+  logo.addEventListener("click", () => displayAllItems(items));
   buttons.addEventListener("click", (event) => onButtonClick(event, items));
 }
-// main
-loadItems()
+
+function onButtonClick(event, items) {
+  const key = event.target.dataset.key;
+  const value = event.target.dataset.value;
+
+  updateItem(key, value, items);
+}
+
+function updateItem(key, value, items) {
+  const listItems = document.querySelectorAll(".items li");
+  // console.log(listItems);
+  items.forEach((item, index) => {
+    if (item[key] === value) {
+      listItems.classList.remove("invisible");
+    } else {
+      listItems[index].classList.add("invisible");
+    }
+  });
+}
+loadItems() //
   .then((items) => {
-    displayItems(items);
+    displayAllItems(items);
     setEventListener(items);
   })
   .catch(console.log("error"));
